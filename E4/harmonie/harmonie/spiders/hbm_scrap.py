fc159f2e43317bc8823b368f6304cb2b6a31f7c5
Stéphanie Parcollet <st.parcollet@orange.fr>
2025-02-26 13:59:06 +0100

    version avec playwright

--- E4/harmonie/harmonie/spiders/hbm_scrap.py
+++ E4/harmonie/harmonie/spiders/hbm_scrap.py
@@ -1,4 +1,12 @@
+import sys
+import os
 import scrapy
+from scrapy.crawler import CrawlerProcess
+from playwright.sync_api import sync_playwright
+from scrapy.http import HtmlResponse
+
+
+sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '../..')))
 from harmonie.items import HarmonieItem
 
 class HbmScrapSpider(scrapy.Spider):
@@ -6,7 +14,14 @@ class HbmScrapSpider(scrapy.Spider):
     allowed_domains = ["musicshopeurope.fr"]
     start_urls = ["https://musicshopeurope.fr/partitions/band/orchestre-d-harmonie/"] 
 
+    def __init__(self, *args, **kwargs):
+        super(HbmScrapSpider, self).__init__(*args, **kwargs)
+        self.playwright = sync_playwright().start()
+        # self.browser = self.playwright.chromium.launch(headless=False)
+        self.browser = self.playwright.firefox.launch(headless=True)
+
     def parse(self, response):
+        print("coucou parse")
         partitions = response.xpath("//a[@class='product-title']")
         for partition in partitions:
             if partition.xpath("./following-sibling::div[@class='product-attributes']/span[contains(text(), 'Set')]"):
@@ -23,43 +38,64 @@ class HbmScrapSpider(scrapy.Spider):
 
 
     def parse_partition(self, response):
-        item = HarmonieItem()
-        item["titre"]           = response.xpath("//h1/text()").get()
-        item["sous_titre"]      = response.xpath("//td[contains(text(), 'Subtitle')]/following-sibling::td/text()").get()
-        item["compositeur"]     = response.xpath("//td[contains(text(), 'Compositeur')]/following-sibling::td/text()").getall()
-        item["arrangeur"]       = response.xpath("//td[contains(text(), 'Arrangeur')]/following-sibling::td/text()").getall()
-        item["artiste"]         = response.xpath("//td[contains(text(), 'Artiste')]/following-sibling::td/text()").getall()
-        item["edition"]         = response.xpath("//td[contains(text(), 'Édition musicale')]/following-sibling::td/text()").get()
-        item["instrumentation"] = response.xpath("//td[contains(text(), 'Instrumentation')]/following-sibling::td/text()").get()
-        item["niveau"]          = response.xpath("//td[contains(text(), 'Grade')]/following-sibling::td/text()").get()
-        item["annee_sortie"]    = response.xpath("//td[contains(text(), 'Année')]/following-sibling::td/text()").get()   
-        item["partie_euro"]     = response.xpath("//td[contains(text(), 'European')]/following-sibling::td/text()").get()     
-        item["genre"]           = response.xpath("//td[contains(text(), 'Genre')]/following-sibling::td/text()").get()
-        item["style"]           = response.xpath("//td[contains(text(), 'Style')]/following-sibling::td/text()").get()
-        item["ISMN"]            = response.xpath("//td[contains(text(), 'ISMN')]/following-sibling::td/text()").get()
-        item["collection"]      = response.xpath("//td[contains(text(), 'Collection')]/following-sibling::td/text()").get()
-        item["ref_editeur"]     = response.xpath("//td[contains(text(), 'No.')]/following-sibling::td/text()").get()
-        item["duree"]           = response.xpath("//td[contains(text(), 'Duration')]/following-sibling::td/text()").get()
-        item["description"]     = response.xpath("//div[@itemprop='description']/text()").get()
-        item["url"]             = response.url
+        print("coucou parse_partition")
+        # Utiliser Playwright pour interagir avec les éléments dynamiques
+        onglet = self.browser.new_page()
+        onglet.goto(response.url)
 
-        # print("type titre           ",type(item["titre"]))
-        # print("type sous_titre      ",type(item["sous_titre"]))
-        # print("type compositeur     ",type(item["compositeur"]))
-        # print("type arrangeur       ",type(item["arrangeur"]))
-        # print("type artiste         ",type(item["artiste"]))
-        # print("type edition         ",type(item["edition"]))
-        # print("type instrumentation ",type(item["instrumentation"]))
-        # print("type niveau          ",type(item["niveau"]))
-        # print("type annee_sortie    ",type(item["annee_sortie"]))
-        # print("type partie_euro     ",type(item["partie_euro"]))
-        # print("type genre           ",type(item["genre"]))
-        # print("type style           ",type(item["style"]))
-        # print("type ISMN            ",type(item["ISMN"]))
-        # print("type collection      ",type(item["collection"]))
-        # print("type ref_editeur     ",type(item["ref_editeur"]))
-        # print("type duree           ",type(item["duree"]))
-        # print("type description     ",type(item["description"]))
-        # print("type url             ",type(item["url"]))
+        # Exemple : Cliquer sur un bouton pour charger du contenu dynamique
+        # onglet.click('text=description')
+        # onglet.wait_for_selector('css=#ui-id-3')
+        onglet.wait_for_selector('a.ui-tabs-anchor[href="#description"]')
+        onglet.click('a.ui-tabs-anchor[href="#description"]')
+        print("onglet",onglet)
+        # Extraire le contenu dynamique
+        content = onglet.text_content('css=#ui-id-3')
+        self.log(f'Dynamic Content: {content}')
+        print("content", content)
+        # Obtenir le contenu HTML rendu
+        body = onglet.content()
+        response_html = HtmlResponse(url=response.url, body=body, encoding='utf-8')
+
+        item = HarmonieItem()
+        item["titre"]           = response_html.xpath("//h1/text()").get()
+        # item["sous_titre"]      = response_html.xpath("//td[contains(text(), 'Subtitle')]/following-sibling::td/text()").get()
+        # item["compositeur"]     = response.xpath("//td[contains(text(), 'Compositeur')]/following-sibling::td/text()").getall()
+        # item["arrangeur"]       = response.xpath("//td[contains(text(), 'Arrangeur')]/following-sibling::td/text()").getall()
+        # item["artiste"]         = response.xpath("//td[contains(text(), 'Artiste')]/following-sibling::td/text()").getall()
+        # item["edition"]         = response.xpath("//td[contains(text(), 'Édition musicale')]/following-sibling::td/text()").get()
+        # item["instrumentation"] = response.xpath("//td[contains(text(), 'Instrumentation')]/following-sibling::td/text()").get()
+        # item["niveau"]          = response.xpath("//td[contains(text(), 'Grade')]/following-sibling::td/text()").get()
+        # item["annee_sortie"]    = response.xpath("//td[contains(text(), 'Année')]/following-sibling::td/text()").get()   
+        # item["partie_euro"]     = response.xpath("//td[contains(text(), 'European')]/following-sibling::td/text()").get()     
+        # item["genre"]           = response.xpath("//td[contains(text(), 'Genre')]/following-sibling::td/text()").get()
+        # item["style"]           = response.xpath("//td[contains(text(), 'Style')]/following-sibling::td/text()").get()
+        # item["ISMN"]            = response.xpath("//td[contains(text(), 'ISMN')]/following-sibling::td/text()").get()
+        # item["collection"]      = response.xpath("//td[contains(text(), 'Collection')]/following-sibling::td/text()").get()
+        # item["ref_editeur"]     = response.xpath("//td[contains(text(), 'No.')]/following-sibling::td/text()").get()
+        # item["duree"]           = response.xpath("//td[contains(text(), 'Duration')]/following-sibling::td/text()").get()
+        item["description"]     = response_html.xpath("//div[@class='description fr-view']/text()").get()
+        # item["url"]             = response.url
 
         yield item
+
+        # Fermer la page
+        onglet.close()
+
+    def closed(self, reason):
+        self.browser.close()
+        self.playwright.stop()
+
+# Exécuter le spider
+if __name__ == "__main__":
+    process = CrawlerProcess(settings={
+        'LOG_LEVEL': 'INFO',
+        'FEEDS': {
+            '../essai4.csv': {
+                'format': 'csv',
+                'overwrite': True,
+            },
+        },
+    })
+    process.crawl(HbmScrapSpider)
+    process.start()
