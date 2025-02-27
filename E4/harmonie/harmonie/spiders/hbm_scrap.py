fc66b1ba8ad65b6923e421e2c22c66f68937e20c
Stéphanie Parcollet <st.parcollet@orange.fr>
2025-02-27 08:44:04 +0100

    ajout description complète

--- E4/harmonie/harmonie/spiders/hbm_scrap.py
+++ E4/harmonie/harmonie/spiders/hbm_scrap.py
@@ -1,10 +1,11 @@
+import re
 import scrapy
 from harmonie.items import HarmonieItem
 
 class HbmScrapSpider(scrapy.Spider):
     name = "hbm_scrap"
     allowed_domains = ["musicshopeurope.fr"]
-    start_urls = ["https://musicshopeurope.fr/partitions/band/orchestre-d-harmonie/"] 
+    start_urls = ["https://musicshopeurope.fr/partitions/band/orchestre-d-harmonie/?page=1"] 
 
     def parse(self, response):
         partitions = response.xpath("//a[@class='product-title']")
@@ -13,10 +14,14 @@ class HbmScrapSpider(scrapy.Spider):
                 partition_url = partition.xpath("./@href").get()
                 yield response.follow(partition, callback=self.parse_partition)
             
-        nombre_pages = response.xpath("//ul[@class='pager-list reset']/li//a/text()").getall()[-1]
-        nombre_pages = '2'
-
-        next_page = f'https://www.musicshopeurope.fr/partitions/band/orchestre-d-harmonie/?page={nombre_pages}'
+        nombre_pages = int(response.xpath("//ul[@class='pager-list reset']/li//a/text()").getall()[-1])
+        # nombre_pages = '2'
+        numero_page_actuelle = 1
+        if re.findall(r'page=(\d+)', response.url)[0] and int(re.findall(r'page=(\d+)', response.url)[0]) < nombre_pages:
+            numero_page_actuelle = int(re.findall(r'page=(\d+)', response.url)[0])
+            numero_page_suivante = numero_page_actuelle + 1
+        
+        next_page = f'https://www.musicshopeurope.fr/partitions/band/orchestre-d-harmonie/?page={numero_page_suivante}'
 
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse)
@@ -31,35 +36,16 @@ class HbmScrapSpider(scrapy.Spider):
         item["artiste"]         = response.xpath("//td[contains(text(), 'Artiste')]/following-sibling::td/text()").getall()
         item["edition"]         = response.xpath("//td[contains(text(), 'Édition musicale')]/following-sibling::td/text()").get()
         item["instrumentation"] = response.xpath("//td[contains(text(), 'Instrumentation')]/following-sibling::td/text()").get()
-        item["niveau"]          = response.xpath("//td[contains(text(), 'Grade')]/following-sibling::td/text()").get()
+        item["niveau"]          = response.xpath("//td[contains(text(), 'Grade')]/following-sibling::td/text()|//td[contains(text(),'Moeilijkheidsgraad orkest')]/following-sibling::td/text()").get()
         item["annee_sortie"]    = response.xpath("//td[contains(text(), 'Année')]/following-sibling::td/text()").get()   
-        item["partie_euro"]     = response.xpath("//td[contains(text(), 'European')]/following-sibling::td/text()").get()     
+        item["partie_euro"]     = response.xpath("//td[contains(text(), 'Europe')]/following-sibling::td/text()").get()     
         item["genre"]           = response.xpath("//td[contains(text(), 'Genre')]/following-sibling::td/text()").get()
         item["style"]           = response.xpath("//td[contains(text(), 'Style')]/following-sibling::td/text()").get()
         item["ISMN"]            = response.xpath("//td[contains(text(), 'ISMN')]/following-sibling::td/text()").get()
         item["collection"]      = response.xpath("//td[contains(text(), 'Collection')]/following-sibling::td/text()").get()
         item["ref_editeur"]     = response.xpath("//td[contains(text(), 'No.')]/following-sibling::td/text()").get()
-        item["duree"]           = response.xpath("//td[contains(text(), 'Duration')]/following-sibling::td/text()").get()
-        item["description"]     = response.xpath("//div[@itemprop='description']/text()").get()
+        item["duree"]           = response.xpath("//td[contains(text(), 'Duration')]/following-sibling::td/text()|//td[contains(text(), 'Tiijdsduur')]/following-sibling::td/text()").get()
+        item["description"]     = response.xpath("//meta[@name='description']/@content").get()
         item["url"]             = response.url
-
-        # print("type titre           ",type(item["titre"]))
-        # print("type sous_titre      ",type(item["sous_titre"]))
-        # print("type compositeur     ",type(item["compositeur"]))
-        # print("type arrangeur       ",type(item["arrangeur"]))
-        # print("type artiste         ",type(item["artiste"]))
-        # print("type edition         ",type(item["edition"]))
-        # print("type instrumentation ",type(item["instrumentation"]))
-        # print("type niveau          ",type(item["niveau"]))
-        # print("type annee_sortie    ",type(item["annee_sortie"]))
-        # print("type partie_euro     ",type(item["partie_euro"]))
-        # print("type genre           ",type(item["genre"]))
-        # print("type style           ",type(item["style"]))
-        # print("type ISMN            ",type(item["ISMN"]))
-        # print("type collection      ",type(item["collection"]))
-        # print("type ref_editeur     ",type(item["ref_editeur"]))
-        # print("type duree           ",type(item["duree"]))
-        # print("type description     ",type(item["description"]))
-        # print("type url             ",type(item["url"]))
 
         yield item
