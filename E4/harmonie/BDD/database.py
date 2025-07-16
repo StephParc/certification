f0a1b0f51c8b0203546c7b5c3584ee83a716bfa0
Stéphanie Parcollet <st.parcollet@orange.fr>
2025-07-16 21:48:07 +0200

    ajout connexion postgreSQL

--- E4/harmonie/BDD/database.py
+++ E4/harmonie/BDD/database.py
@@ -8,8 +8,18 @@ env_path = os.path.join(os.path.dirname(__file__),'.env')
 load_dotenv(dotenv_path=env_path)
 
 def get_engine():
-    SQL_DATABASE_URL = os.getenv('SQL_DATABASE_URL')
-    engine = create_engine(SQL_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+    ## Pour une BDD SQLite: 
+    # SQL_DATABASE_URL = os.getenv('SQL_DATABASE_URL')
+    # engine = create_engine(SQL_DATABASE_URL, connect_args={"check_same_thread": False}, echo=True)
+
+    # Pour une BDD PostgreSQL:
+    dbhost = os.getenv('DBHOST', 'localhost')
+    dbname = os.getenv('DBNAME', 'mydatabase')
+    dbuser = os.getenv('DBUSER', 'myuser')
+    password = os.getenv('PASSWORD', 'mypassword')
+    SQL_DATABASE_URL = f"postgresql://{dbuser}:{password}@{dbhost}:5432/{dbname}"
+
+    engine = create_engine(SQL_DATABASE_URL, echo=True)
     return engine
 
 def sql_connect():
